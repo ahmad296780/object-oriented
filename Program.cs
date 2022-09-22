801e7352214304516c01a48086128b30e9d845c5
ahmad <bha296780@gmail.com>
2022-09-22 12:25:58 -0400

    final

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+
+using Object_oriented;
+
+Car car1 = new Car();
+car1.Make = "Honda";
+car1.Model = "Accord";
+car1.Year = 2020;
+Console.WriteLine(car1.Make);
+Console.WriteLine(car1.Model);
+Console.WriteLine(car1.Year);
+
+
